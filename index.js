cc200cf5f0cb81a674fbc681885d5305f9bd3447
AmrMashaal <115498987+AmrMashaal@users.noreply.github.com>
2023-04-27 03:37:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ let one = document.querySelector(".one");
 let reset = document.querySelector(".reset");
 let span = document.querySelector("span");
 
-one.onclick = function (){
-    ++span.innerHTML
-}
+one.onclick = function () {
+  ++span.innerHTML;
+  window.localStorage.setItem("number", span.innerHTML);
+};
 
-reset.onclick = function (){
-    span.innerHTML = "0" 
+reset.onclick = function () {
+  span.innerHTML = "0";
+};
+
+if (window.localStorage.getItem("number")) {
+  span.innerHTML = window.localStorage.getItem("number");
 }
